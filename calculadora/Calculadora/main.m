99e827dcfbaee9ead08fe759c23f5a42b3cc0549
Gil Verduzco <gilverduzco@MacBook-Pro-de-Gil.local>
2014-08-15 11:19:47 -0600

    SubidaInicial

---
+++ calculadora/Calculadora/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Calculadora
+//
+//  Created by Gil Verduzco on 15/08/14.
+//  Copyright (c) 2014 gilito. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
